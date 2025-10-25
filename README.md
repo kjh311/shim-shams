36c72b21642bbc91d585340d34d378ec6e50dc99
kjh311 <kjh311@gmail.com>
2025-10-25 17:50:06 -0600

    skeleton

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Shim Sham Band Webpage
-readme
--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 $(document).ready(function () {
   $(".dropdown-toggle").dropdown();
 });
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Find the element with the ID "copyright-year" and set its content
+document.getElementById(
+  "copyright-year"
+).textContent = ` © Shim Shams ${currentYear}`;
